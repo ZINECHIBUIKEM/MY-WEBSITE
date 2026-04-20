f5e7a139be694151e73fd0925497f9da45faebe6
ETUKEMKA CHIBUIKEM <etukemkachibuikem@gmail.com>
2026-04-21 00:24:55 +0100

    Made changes to hover and parralax speeds of Portfolie element

--- src/components/PortfolioDisplay.jsx
+++ src/components/PortfolioDisplay.jsx
@@ -22,7 +22,7 @@ export function PortfolioDisplay({ lightmode }) {
   // PARALLAX SPEEDS
   const y1 = useTransform(scrollYProgress, [0, 1], [0, -60]);
   const y2 = useTransform(scrollYProgress, [0, 1], [0, -100]);
-  const y3 = useTransform(scrollYProgress, [0, 1], [0, -280]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, -210]);
 
   return (
     <div
@@ -35,19 +35,19 @@ export function PortfolioDisplay({ lightmode }) {
         <div className="flex flex-col gap-[5vh] items-center justify-between h-[50vh] w-[30vw] shrink-0">
 
           <motion.div style={{ y: y1 }} className="transition-all duration-300 min-w-35 max-w-70">
-            <img src={Weather1} className="rounded-3xl hover:scale-105" />
+            <img src={Weather1} className="rounded-3xl" />
           </motion.div>
 
           <motion.div style={{ y: y1 }} className="transition-all duration-300 min-w-35 max-w-70">
-            <img src={Blue2} className="rounded-3xl hover:scale-105" />
+            <img src={Blue2} className="rounded-3xl" />
           </motion.div>
 
           <motion.div style={{ y: y1 }} className="transition-all duration-300 min-w-35 max-w-70">
-            <img src={NW3} className="rounded-3xl hover:scale-105" />
+            <img src={NW3} className="rounded-3xl -105" />
           </motion.div>
 
           <motion.div style={{ y: y1 }} className="transition-all duration-300 min-w-35 max-w-70">
-            <img src={Weather1} className="rounded-3xl hover:scale-105" />
+            <img src={Weather1} className="rounded-3xl -105" />
           </motion.div>
         </div>
       </a>
@@ -56,19 +56,19 @@ export function PortfolioDisplay({ lightmode }) {
       <a href="https://zinechibuikem.github.io/BLUE-MAIL-PROJECT/" target="_blank">
         <div className="flex flex-col gap-[5vh] items-center justify-between h-[50vh] w-[30vw] shrink-0">
 
-          <motion.div style={{ y: y3 }} className="transition-all duration-300 min-w-35 max-w-70 hover:scale-105">
+          <motion.div style={{ y: y3 }} className="transition-all duration-300 min-w-35 max-w-70 -105">
             <img src={Blue1} className="rounded-3xl" />
           </motion.div>
 
-          <motion.div style={{ y: y3 }} className="transition-all duration-300 min-w-35 max-w-70 hover:scale-105">
+          <motion.div style={{ y: y3 }} className="transition-all duration-300 min-w-35 max-w-70 -105">
             <img src={Weather2} className="rounded-3xl" />
           </motion.div>
 
-          <motion.div style={{ y: y3 }} className="transition-all duration-300 min-w-35 max-w-70 hover:scale-105">
+          <motion.div style={{ y: y3 }} className="transition-all duration-300 min-w-35 max-w-70 -105">
             <img src={NW3} className="rounded-3xl" />
           </motion.div>
 
-          <motion.div style={{ y: y3 }} className="transition-all duration-300 min-w-35 max-w-70 hover:scale-105">
+          <motion.div style={{ y: y3 }} className="transition-all duration-300 min-w-35 max-w-70 -105">
             <img src={Blue1} className="rounded-3xl" />
           </motion.div>
         </div>
@@ -78,19 +78,19 @@ export function PortfolioDisplay({ lightmode }) {
       <a href="https://weather-app-zinecast.vercel.app/" target="_blank">
         <div className="flex flex-col gap-[5vh] items-center justify-between h-[50vh] w-[30vw] shrink-0">
 
-          <motion.div style={{ y: y2 }} className="transition-all duration-300 min-w-35 max-w-70 hover:scale-105">
+          <motion.div style={{ y: y2 }} className="transition-all duration-300 min-w-35 max-w-70 -105">
             <img src={NW1} className="rounded-3xl" />
           </motion.div>
 
-          <motion.div style={{ y: y2 }} className="transition-all duration-300 min-w-35 max-w-70 hover:scale-105">
+          <motion.div style={{ y: y2 }} className="transition-all duration-300 min-w-35 max-w-70 -105">
             <img src={Weather2} className="rounded-3xl" />
           </motion.div>
 
-          <motion.div style={{ y: y2 }} className="transition-all duration-300 min-w-35 max-w-70 hover:scale-105">
+          <motion.div style={{ y: y2 }} className="transition-all duration-300 min-w-35 max-w-70 -105">
             <img src={Blue3} className="rounded-3xl" />
           </motion.div>
 
-          <motion.div style={{ y: y2 }} className="transition-all duration-300 min-w-35 max-w-70 hover:scale-105">
+          <motion.div style={{ y: y2 }} className="transition-all duration-300 min-w-35 max-w-70 -105">
             <img src={NW1} className="rounded-3xl" />
           </motion.div>
         </div>
